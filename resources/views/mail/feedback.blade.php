7b01c9dc3eb46e37bc90cd3b92e87c8ae8c2bad9
Keithley Meade <keithleym89@gmail.com>
2024-02-24 18:29:56 -0400

    final updated files

--- resources/views/mail/feedback.blade.php
+++ resources/views/mail/feedback.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Feedback Received</title>
-</head>
-<body>
-    <h1>Feedback Received</h1>
-    <p><strong>Name:</strong> {{ $name }}</p>
-    <p><strong>Email:</strong> {{ $email }}</p>
-    <p><strong>Comments:</strong> {{ $comment }}</p>
-</body>
-</html>
+@extends('layouts.email')
+@section('content')
+<p>{{ $name }} has sent feedback about your website. Below are the comments left by
+<p>{{ $comment }}</p>
+@endsection
