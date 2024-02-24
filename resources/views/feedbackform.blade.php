d09d493709a4e69d4a0cc229d3ce7e5cd0d3be49
Keithley Meade <keithleym89@gmail.com>
2024-02-24 17:56:34 -0400

    final updated files

--- resources/views/feedbackform.blade.php
+++ resources/views/feedbackform.blade.php
@@ -6,15 +6,15 @@
     <form action="{{ url('/feedback/send') }}" method="POST">
         @csrf <!-- CSRF Token -->
         <div class="form-group">
-            <label for="full-name">Full Name (Required)</label>
+            <label for="full-name">Full Name <span style="color: red;">(Required)</span></label>
             <input type="text" class="form-control" id="full-name" name="full_name" required>
         </div>
         <div class="form-group">
-            <label for="email">Email (Required)</label>
+            <label for="email">Email <span style="color: red;">(Required)</span></label>
             <input type="email" class="form-control" id="email" name="email" required>
         </div>
         <div class="form-group">
-            <label for="comments">Comments (Required)</label>
+            <label for="comments">Comments <span style="color: red;">(Required)</span></label>
             <textarea class="form-control" id="comments" name="comments" required></textarea>
         </div>
         <p>Let us know what you think of our website.</p>
