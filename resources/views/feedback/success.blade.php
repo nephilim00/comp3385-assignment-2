040c2abef3ad98024518b4e0479dd4826f344d73
Keithley Meade <keithleym89@gmail.com>
2024-02-24 22:08:37 -0400

    final updated files

--- resources/views/feedback/success.blade.php
+++ resources/views/feedback/success.blade.php
@@ -1,16 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-        <link href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" rel="stylesheet">
-            <div class="text-center" style="background-color: #ffffff; border: 2px solid #28a745; color: #28a745; border-radius: .25rem; padding: 1rem; margin-top: 2rem;">
-                <h4 class="alert-heading" style="font-weight: 3000;"><i class="fas fa-check-circle"></i></h4>
-                <h4 class="alert-heading" style="font-weight: 3000; margin: 0;">Success!</h4>
-                <p style="font-size: 18px; color: #000000;">Thank you for your feedback. We will review your feedback and get back to you.</p>
+    <div class="container">
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <div class="border rounded border-success p-5">
+                <div class="text-center"> 
+   
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-check-circle text-success" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                        <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
+                    </svg>   
+                    <h2 class="text-success fw-bold">  Success! </h2>
+                
+                Thank you for your feedback. We will review your feedback and get back to you.
+                </div>
             </div>
-        </div>
     </div>
+    
 </div>
-@endsection
+
+    
+</div>
+@endsection 
